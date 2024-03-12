8f252e4626825bc246df712d47affc390520c711
Navneet Desai <navneetdesai44@gmail.com>
2024-03-12 02:45:31 -0400

    push updates

---
+++ littlelemon/restaurant/tests/test_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from ..models import Menu
+# from littlelemon.restaurant.models import Menu
+
+
+class MenuViewTest(TestCase):
+
+    def test_getall(self):
+        pass
